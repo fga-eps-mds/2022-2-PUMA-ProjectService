d1633e8ba8cd18e4128b7661a99eb4cc84f85719
Rafaelltm <rafaelltm10@hotmail.com>
2023-04-30 17:06:12 -0300

    Ajustando inicializacao do sequelize

--- src/index.js
+++ src/index.js
@@ -7,11 +7,22 @@ const environment = require('./config/environment.config');
 const configRoutes = require('./routes/router');
 
 // eslint-disable-next-line no-unused-vars
-const db = require('../dbconfig/dbConfig');
+// const db = require('../dbconfig/dbConfig');
 
 environment.configEnv();
 
 const app = express();
+
+(async () => {
+  const database = require('./db/AppDb');
+  try {
+    const resultado = await database.sync();
+    console.log(resultado);
+  } catch (error) {
+    console.log("Erro ao inicializar o banco ->", error);
+  }
+})();
+
 app.disable('x-powered-by');
 const corsOptions = {
   // origin: `${global.URL_API}`,
